4df9967264348a48c68ef2892e5b4bdd57c4bf12
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-26 12:29:55 +0000

    Adding tarfile member sanitization to extractall()

--- benchmarks/benchmark_script.py
+++ benchmarks/benchmark_script.py
@@ -303,7 +303,26 @@ if __name__ == '__main__':
     if args.dataset == 'sift' and not os.path.exists(SIFT_DIR):
         download_file("ftp://ftp.irisa.fr/local/texmex/corpus/sift.tar.gz", "datasets")
         with tarfile.open("datasets/sift.tar.gz") as t:
-            t.extractall(path="datasets")
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(t, path="datasets")
         subprocess.call("python datasets/convert_texmex_fvec.py datasets/sift/sift_base.fvecs >> datasets/sift.txt", shell=True)
 
     if args.dataset == 'youtube' and not os.path.exists(YOUTUBE_DIR):
